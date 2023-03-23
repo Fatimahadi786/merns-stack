8b4d94ff05ef8a1e7997f3780944328ea7c80652
fatima hadi <fatimahadi151214@gmail.com>
2023-03-23 02:04:49 -0700

    add file

---
+++ js/amOperation.js
@@ -0,0 +1,23 @@
+
+let   x,y,add,sub,mul,div,mol,avg;
+x=10;
+y=45;
+//addition
+add=x+y;
+console.log(add)
+//subtraction
+sub=x-y;
+console.log(sub)
+//multiplication
+mul=x*y;
+console.log(mul)
+//divison
+div=x/y;
+console.log(div)
+//modulus
+mol=x%y;
+console.log(mol)
+//average
+avg=((x+y)/2);
+console.log(avg)
+
